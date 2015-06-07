689ec99e9ef1ee02ad478eeafc45b2e03638b1a6
Selina <enigmacat@users.noreply.github.com>
2015-06-07 19:02:31 -0400

    obstacle infinite generation

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -8,17 +8,28 @@ class MainScene: CCNode {
     var _ground2: CCSprite!
     var grounds: [CCSprite] = []
     var sinceTouch: CCTime = 0
+    var obstacles: [CCNode] = []
+    let firstObstaclePosition : CGFloat = 280
+    let _distanceBetweenObstacles: CGFloat = 160
     
     func didLoadFromCCB() {
         self.userInteractionEnabled = true
         grounds.append(_ground1)
         grounds.append(_ground2)
+        
+        self.spawnNewObstacle()
+        self.spawnNewObstacle()
+        self.spawnNewObstacle()
     }
     
     override func update(delta: CCTime) {
+        //move the fly
         _hero.position = ccp(_hero.position.x + _scrollSpeed * CGFloat(delta), _hero.position.y)
+        
+        //update physics node position
         _physicsNode.position = ccp(_physicsNode.position.x - _scrollSpeed * CGFloat(delta), _physicsNode.position.y)
         
+        //handle ground scrolling so that the fly doesn't fall off the screen
         for ground in grounds {
             let groundWorldPosition = _physicsNode.convertToWorldSpace(ground.position)
             let groundScreenPosition = self.convertToNodeSpace(groundWorldPosition)
@@ -27,11 +38,14 @@ class MainScene: CCNode {
             }
         }
         
+        //limit fly's velocity
         let velocityY = clampf(Float(_hero.physicsBody.velocity.y), -Float(CGFloat.max), 200)
         _hero.physicsBody.velocity = ccp(0, CGFloat(velocityY))
         
+        //rotate the fly down
         sinceTouch += delta
         _hero.rotation = clampf(_hero.rotation, -30,90)
+        
         if (_hero.physicsBody.allowsRotation) {
             let angularVelocity = clampf(Float(_hero.physicsBody.angularVelocity),2,-1)
             _hero.physicsBody.angularVelocity = CGFloat(angularVelocity)
@@ -40,11 +54,35 @@ class MainScene: CCNode {
             let impulse  = -20000 * delta
             _hero.physicsBody.applyAngularImpulse(CGFloat(impulse))
         }
+        
+        for obstacle in obstacles.reverse() {
+            let obstacleWorldPosition = _physicsNode.convertToWorldSpace(obstacle.position)
+            let obstacleScreenPosition = self.convertToNodeSpace(obstacleWorldPosition)
+            
+            if obstacleScreenPosition.x < -obstacle.contentSize.width {
+                obstacle.removeFromParent()
+                obstacles.removeAtIndex(find(obstacles, obstacle)!)
+                
+                self.spawnNewObstacle()
+            }
+        }
     }
     
     override func touchBegan(touch: CCTouch!, withEvent event:CCTouchEvent!) {
         _hero.physicsBody.applyImpulse(ccp(0,400))
         _hero.physicsBody.applyAngularImpulse(10000)
         sinceTouch = 0
+    }
+    
+    func spawnNewObstacle() {
+        var prevObstaclePos = firstObstaclePosition
+        if obstacles.count > 0 {
+            prevObstaclePos = obstacles.last!.position.x
+        }
+        
+        let obstacle = CCBReader.load("Obstacle")
+        obstacle.position = ccp(prevObstaclePos + _distanceBetweenObstacles, 0)
+        _physicsNode.addChild(obstacle)
+        obstacles.append(obstacle)
     }
 }
